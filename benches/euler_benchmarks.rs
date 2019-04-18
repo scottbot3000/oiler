2f5ecfd35677c9f55e40e7859a5bc3ef3ddcd623
scottbot3000 <scott.carbonbased@gmail.com>
2019-04-17 18:00:44 -0700

    Refactor project to include a binary and library for use in benchmarks.

--- benches/euler_benchmarks.rs
+++ benches/euler_benchmarks.rs
@@ -1,11 +1,14 @@
-// #[macro_use]
-// extern crate criterion;
+#[macro_use]
+extern crate criterion;
 
-// use criterion::Criterion;
+use criterion::Criterion;
+use oiler::euler::*;
+
+fn e1_bench(c: &mut Criterion) {
+    c.bench_function("fib 20", |b| b.iter(|| euler_1()));
+}
 
 
 
-
-
-//criterion_group!(benches, criterion_benchmark);
-//criterion_main!(benches);
+criterion_group!(benches, e1_bench);
+criterion_main!(benches);
